b970933e2008658019eaf7b817e80e2f2205be98
Kristján Pétursson <kristjan@gmail.com>
2019-09-29 16:32:35 -0700

    Sort Groups and Scenes section of app list
    Seems like these should be two different apps, but since they're
    together, splitting up the Groups from the Scenes makes it a bit easier
    to deal with.

--- hubihud.js
+++ hubihud.js
@@ -4,9 +4,47 @@ function dispatch() {
 		case /\/logs.*/.test(path):
 			logs();
 			break;
+		case /\/installedapp\/list/.test(path):
+			appList();
+			break;
 		default:
 			break;
 	}
+}
+
+function appList() {
+	sortGroupsAndScenes();
+}
+
+function sortGroupsAndScenes() {
+	namesContainer = $('#app-table td[data-order="Groups and Scenes"]');
+	versionsContainer = namesContainer.next();
+
+	names = namesContainer.children('div');
+	versions = versionsContainer.children('div');
+
+	pairs = [];
+	// Start at 1 to skip the headers
+	for (i = 1; i < names.length; i++) {
+		pairs.push([names[i], versions[i]]);
+	}
+
+	pairs.sort(function(a, b) {
+		aVersion = $(a[1]).text();
+		bVersion = $(b[1]).text();
+		return (aVersion < bVersion) ? -1 : (aVersion > bVersion) ? 1 : 0;
+	});
+
+	names = [];
+	versions = [];
+
+	for (i = 0; i < pairs.length; i++) {
+		names.push(pairs[i][0]);
+		versions.push(pairs[i][1]);
+	}
+
+	namesContainer.append(names);
+	versionsContainer.append(versions);
 }
 
 function isHubitat() {
